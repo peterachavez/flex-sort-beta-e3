575c9775aa07e6bf7ac370aedcee77abba5cdf23
Peter Chavez <pac2206@tc.columbia.edu>
2025-08-07 13:56:47 -0400

    Fix: removed next/navigation and used window.location

--- src/components/PricingTiers.tsx
+++ src/components/PricingTiers.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import React, { useState } from 'react'
-import { useRouter } from 'next/navigation'
 import { CheckCircle, Check } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
